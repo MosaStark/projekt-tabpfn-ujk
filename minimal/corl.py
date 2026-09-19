9d6ff6b35ea9e941e43eac95f5e9c1243ef8242d
tjacek <jacekt89@gmail.com>
2026-09-19 13:20:23 +0200

    show corelation

--- minimal/corl.py
+++ minimal/corl.py
@@ -1,6 +1,7 @@
 import numpy as np
 from dataclasses import dataclass
 from collections import defaultdict
+from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
 import base
 
@@ -30,22 +31,20 @@ def corl_plot(in_path):
 	matrices=[ ShapleyMatrix.from_path(path_i)
 	            for path_i in base.top_files(in_path)]
 	shap_dict=defaultdict(dict)
-#	{ "RF":{},"TabPFN":{}}
 	for matrix_i in matrices:
 		shap_dict[matrix_i.data][matrix_i.clf]=matrix_i
+	plt.rcParams.update({'font.size': 12})
 	for key_i,value_i in shap_dict.items():
 		x=value_i["RF"].as_arr()
 		y=value_i["TabPFN"].as_arr()
+		r, p = pearsonr(x, y)
 		plt.scatter(x, y, color="steelblue", edgecolor="black", alpha=0.7)
-		plt.xlabel("RF")
+		text=f"\nPearson correlation: r = {r:.4f}, p = {p:.3e}"
+		plt.xlabel("RF"+text)
 		plt.ylabel("TabPFN")
 		plt.title(key_i)
+		plt.tight_layout()
 		plt.show()
-#plt.xlabel("Zmienna X")
-#plt.ylabel("Zmienna Y")
-#plt.title("Scatter plot dwóch zmiennych")
-#plt.grid(True, linestyle="--", alpha=0.4)
- 
-#plt.show()
+
 corl_plot("output/matrix")
 
